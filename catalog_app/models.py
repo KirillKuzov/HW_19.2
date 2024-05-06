184f37371d4ecc6d4ab4a4ce02b80519efe78fee
К <hitandrun444@yandex.ru>
2024-05-06 16:23:41 +0300

    add files

--- catalog_app/models.py
+++ catalog_app/models.py
@@ -18,7 +18,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование продукта')
     description = models.TextField(verbose_name='Описание продукта', **NULLABLE)
-    picture = models.ImageField(upload_to='products/', verbose_name='Превью')
+    picture = models.ImageField(upload_to='products/', verbose_name='Превью', **NULLABLE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     price = models.IntegerField(verbose_name='Цена за покупку')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
