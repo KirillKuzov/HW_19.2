efbf2313bb3127ed6d4dfbd502ec7603399e2371
К <hitandrun444@yandex.ru>
2024-05-06 11:20:37 +0300

    add files

--- config/settings.py
+++ config/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'catalog_db',
         'USER': 'postgres',
         'PASSWORD': '123444'
